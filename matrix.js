7d67319a1a03b1270dd7d2110960ac704156bdb9
Ha <typ0@naver.com>
2018-09-13 15:54:00 +0900

    detail

--- matrix.js
+++ matrix.js
@@ -54,16 +54,16 @@ function calculate(upgradeLimit, actionNormal, actionHammer) {
     innocentNormal[i] = [];
     for (let j = 0; j <= upgradeLimit; j++) {
       costNormal[i][j] = {
-        upgrade: 0,
-        white: 0,
-        innocent: 0,
-        hammer: 0,
+        upgrade: [],
+        white: [],
+        innocent: [],
+        hammer: [],
       };
       costFinalNormal[i][j] = {
-        upgrade: 0,
-        white: 0,
-        innocent: 0,
-        hammer: 0,
+        upgrade: [],
+        white: [],
+        innocent: [],
+        hammer: [],
       };
       innocentNormal[i][j] = 0;
     }
@@ -75,16 +75,16 @@ function calculate(upgradeLimit, actionNormal, actionHammer) {
     innocentHammer[i] = [];
     for (let j = 0; j <= upgradeLimit + 1; j++) {
       costHammer[i][j] = {
-        upgrade: 0,
-        white: 0,
-        innocent: 0,
-        hammer: 0,
+        upgrade: [],
+        white: [],
+        innocent: [],
+        hammer: [],
       };
       costFinalHammer[i][j] = {
-        upgrade: 0,
-        white: 0,
-        innocent: 0,
-        hammer: 0,
+        upgrade: [],
+        white: [],
+        innocent: [],
+        hammer: [],
       };
       innocentHammer[i][j] = 0;
     }
@@ -94,37 +94,56 @@ function calculate(upgradeLimit, actionNormal, actionHammer) {
     for (let j = upgradeLimit + 1; j >= i; j--) {
       if (actionMatrixHammer[i][j] === 'U') {
         if (actionMatrixHammer[i][j + 1] === 'W') {
-          costHammer[i][j].upgrade =
-            costHammer[i + 1][j + 1].upgrade + 1 / UPGRADE_PERCENTAGE;
-          costHammer[i][j].white =
-            costHammer[i + 1][j + 1].white +
-            (1 - UPGRADE_PERCENTAGE) / UPGRADE_PERCENTAGE;
-          costHammer[i][j + 1].upgrade = costHammer[i][j].upgrade;
-          costHammer[i][j + 1].white = costHammer[i][j].white + 1;
+          costHammer[i][j].upgrade = [
+            ...costHammer[i + 1][j + 1].upgrade,
+            1 / UPGRADE_PERCENTAGE,
+          ];
+          costHammer[i][j].white = [
+            ...costHammer[i + 1][j + 1].white,
+            (1 - UPGRADE_PERCENTAGE) / UPGRADE_PERCENTAGE,
+          ];
+          costHammer[i][j + 1].upgrade = [...costHammer[i][j].upgrade];
+          costHammer[i][j + 1].white = [...costHammer[i][j].white, 1];
           innocentHammer[i][j] = innocentHammer[i + 1][j + 1];
           innocentHammer[i][j + 1] = innocentHammer[i + 1][j + 1];
         } else {
-          costHammer[i][j].upgrade =
-            UPGRADE_PERCENTAGE * costHammer[i + 1][j + 1].upgrade +
-            (1 - UPGRADE_PERCENTAGE) * costHammer[i][j + 1].upgrade +
-            1;
-          costHammer[i][j].white =
-            UPGRADE_PERCENTAGE * costHammer[i + 1][j + 1].white +
-            (1 - UPGRADE_PERCENTAGE) * costHammer[i][j + 1].white;
-          costHammer[i][j].innocent =
-            UPGRADE_PERCENTAGE * costHammer[i + 1][j + 1].innocent +
-            (1 - UPGRADE_PERCENTAGE) * costHammer[i][j + 1].innocent;
-          costHammer[i][j].hammer =
-            UPGRADE_PERCENTAGE * costHammer[i + 1][j + 1].hammer +
-            (1 - UPGRADE_PERCENTAGE) * costHammer[i][j + 1].hammer;
+          costHammer[i][j].upgrade = [
+            ...costHammer[i + 1][j + 1].upgrade.map(
+              x => x * UPGRADE_PERCENTAGE,
+            ),
+            ...costHammer[i][j + 1].upgrade.map(
+              x => x * (1 - UPGRADE_PERCENTAGE),
+            ),
+            1,
+          ];
+          costHammer[i][j].white = [
+            ...costHammer[i + 1][j + 1].white.map(x => x * UPGRADE_PERCENTAGE),
+            ...costHammer[i][j + 1].white.map(
+              x => x * (1 - UPGRADE_PERCENTAGE),
+            ),
+          ];
+          costHammer[i][j].innocent = [
+            ...costHammer[i + 1][j + 1].innocent.map(
+              x => x * UPGRADE_PERCENTAGE,
+            ),
+            ...costHammer[i][j + 1].innocent.map(
+              x => x * (1 - UPGRADE_PERCENTAGE),
+            ),
+          ];
+          costHammer[i][j].hammer = [
+            ...costHammer[i + 1][j + 1].hammer.map(x => x * UPGRADE_PERCENTAGE),
+            ...costHammer[i][j + 1].hammer.map(
+              x => x * (1 - UPGRADE_PERCENTAGE),
+            ),
+          ];
           innocentHammer[i][j] =
             UPGRADE_PERCENTAGE * innocentHammer[i + 1][j + 1] +
             (1 - UPGRADE_PERCENTAGE) * innocentHammer[i][j + 1];
         }
       }
       if (actionMatrixHammer[i][j] === 'I') {
-        innocentHammer[i][j] = 1;
-        costHammer[i][j].innocent = 1;
+        innocentHammer[i][j] = [1];
+        costHammer[i][j].innocent = [1];
       }
     }
   }
@@ -132,44 +151,58 @@ function calculate(upgradeLimit, actionNormal, actionHammer) {
   for (let i = upgradeLimit; i >= 0; i--) {
     for (let j = upgradeLimit; j >= i; j--) {
       if (actionMatrixNormal[i][j] === 'U') {
-        costNormal[i][j].upgrade =
-          UPGRADE_PERCENTAGE * costNormal[i + 1][j + 1].upgrade +
-          (1 - UPGRADE_PERCENTAGE) * costNormal[i][j + 1].upgrade +
-          1;
-        costNormal[i][j].white =
-          UPGRADE_PERCENTAGE * costNormal[i + 1][j + 1].white +
-          (1 - UPGRADE_PERCENTAGE) * costNormal[i][j + 1].white;
-        costNormal[i][j].innocent =
-          UPGRADE_PERCENTAGE * costNormal[i + 1][j + 1].innocent +
-          (1 - UPGRADE_PERCENTAGE) * costNormal[i][j + 1].innocent;
-        costNormal[i][j].hammer =
-          UPGRADE_PERCENTAGE * costNormal[i + 1][j + 1].hammer +
-          (1 - UPGRADE_PERCENTAGE) * costNormal[i][j + 1].hammer;
+        costNormal[i][j].upgrade = [
+          ...costNormal[i + 1][j + 1].upgrade.map(x => x * UPGRADE_PERCENTAGE),
+          ...costNormal[i][j + 1].upgrade.map(
+            x => x * (1 - UPGRADE_PERCENTAGE),
+          ),
+          1,
+        ];
+        costNormal[i][j].white = [
+          ...costNormal[i + 1][j + 1].white.map(x => x * UPGRADE_PERCENTAGE),
+          ...costNormal[i][j + 1].white.map(x => x * (1 - UPGRADE_PERCENTAGE)),
+        ];
+        costNormal[i][j].innocent = [
+          ...costNormal[i + 1][j + 1].innocent.map(x => x * UPGRADE_PERCENTAGE),
+          ...costNormal[i][j + 1].innocent.map(
+            x => x * (1 - UPGRADE_PERCENTAGE),
+          ),
+        ];
+        costNormal[i][j].hammer = [
+          ...costNormal[i + 1][j + 1].hammer.map(x => x * UPGRADE_PERCENTAGE),
+          ...costNormal[i][j + 1].hammer.map(x => x * (1 - UPGRADE_PERCENTAGE)),
+        ];
         innocentNormal[i][j] =
           UPGRADE_PERCENTAGE * innocentNormal[i + 1][j + 1] +
           (1 - UPGRADE_PERCENTAGE) * innocentNormal[i][j + 1];
       }
       if (actionMatrixNormal[i][j] === 'H') {
-        costNormal[i][j].upgrade =
-          HAMMER_PERCENTAGE * costHammer[i][j].upgrade +
-          (1 - HAMMER_PERCENTAGE) * costHammer[i][j + 1].upgrade;
-        costNormal[i][j].white =
-          HAMMER_PERCENTAGE * costHammer[i][j].white +
-          (1 - HAMMER_PERCENTAGE) * costHammer[i][j + 1].white;
-        costNormal[i][j].innocent =
-          HAMMER_PERCENTAGE * costHammer[i][j].innocent +
-          (1 - HAMMER_PERCENTAGE) * costHammer[i][j + 1].innocent;
-        costNormal[i][j].hammer =
-          HAMMER_PERCENTAGE * costHammer[i][j].hammer +
-          (1 - HAMMER_PERCENTAGE) * costHammer[i][j + 1].hammer +
-          1;
+        costNormal[i][j].upgrade = [
+          ...costHammer[i][j].upgrade.map(x => x * HAMMER_PERCENTAGE),
+          ...costHammer[i][j + 1].upgrade.map(x => x * (1 - HAMMER_PERCENTAGE)),
+        ];
+        costNormal[i][j].white = [
+          ...costHammer[i][j].white.map(x => x * HAMMER_PERCENTAGE),
+          ...costHammer[i][j + 1].white.map(x => x * (1 - HAMMER_PERCENTAGE)),
+        ];
+        costNormal[i][j].innocent = [
+          ...costHammer[i][j].innocent.map(x => x * HAMMER_PERCENTAGE),
+          ...costHammer[i][j + 1].innocent.map(
+            x => x * (1 - HAMMER_PERCENTAGE),
+          ),
+        ];
+        costNormal[i][j].hammer = [
+          ...costHammer[i][j].hammer.map(x => x * HAMMER_PERCENTAGE),
+          ...costHammer[i][j + 1].hammer.map(x => x * (1 - HAMMER_PERCENTAGE)),
+          1,
+        ];
         innocentNormal[i][j] =
           HAMMER_PERCENTAGE * innocentHammer[i][j] +
           (1 - HAMMER_PERCENTAGE) * innocentHammer[i][j + 1];
       }
       if (actionMatrixNormal[i][j] === 'I') {
         innocentNormal[i][j] = 1;
-        costNormal[i][j].innocent = 1;
+        costNormal[i][j].innocent = [1];
       }
     }
   }
@@ -178,38 +211,70 @@ function calculate(upgradeLimit, actionNormal, actionHammer) {
     for (let j = upgradeLimit + 1; j >= i; j--) {
       if (actionMatrixHammer[i][j] === 'U') {
         if (actionMatrixHammer[i][j + 1] === 'W') {
-          costFinalHammer[i][j].upgrade =
-            costFinalHammer[i + 1][j + 1].upgrade + 1 / UPGRADE_PERCENTAGE;
-          costFinalHammer[i][j].white =
-            costFinalHammer[i + 1][j + 1].white +
-            (1 - UPGRADE_PERCENTAGE) / UPGRADE_PERCENTAGE;
-          costFinalHammer[i][j + 1].upgrade = costFinalHammer[i][j].upgrade;
-          costFinalHammer[i][j + 1].white = costFinalHammer[i][j].white + 1;
+          costFinalHammer[i][j].upgrade = [
+            ...costFinalHammer[i + 1][j + 1].upgrade,
+            1 / UPGRADE_PERCENTAGE,
+          ];
+          costFinalHammer[i][j].white = [
+            ...costFinalHammer[i + 1][j + 1].white,
+            (1 - UPGRADE_PERCENTAGE) / UPGRADE_PERCENTAGE,
+          ];
+          costFinalHammer[i][j + 1].upgrade = [
+            ...costFinalHammer[i][j].upgrade,
+          ];
+          costFinalHammer[i][j + 1].white = [...costFinalHammer[i][j].white, 1];
         } else {
-          costFinalHammer[i][j].upgrade =
-            UPGRADE_PERCENTAGE * costFinalHammer[i + 1][j + 1].upgrade +
-            (1 - UPGRADE_PERCENTAGE) * costFinalHammer[i][j + 1].upgrade +
-            1;
-          costFinalHammer[i][j].white =
-            UPGRADE_PERCENTAGE * costFinalHammer[i + 1][j + 1].white +
-            (1 - UPGRADE_PERCENTAGE) * costFinalHammer[i][j + 1].white;
-          costFinalHammer[i][j].innocent =
-            UPGRADE_PERCENTAGE * costFinalHammer[i + 1][j + 1].innocent +
-            (1 - UPGRADE_PERCENTAGE) * costFinalHammer[i][j + 1].innocent;
-          costFinalHammer[i][j].hammer =
-            UPGRADE_PERCENTAGE * costFinalHammer[i + 1][j + 1].hammer +
-            (1 - UPGRADE_PERCENTAGE) * costFinalHammer[i][j + 1].hammer;
+          costFinalHammer[i][j].upgrade = [
+            ...costFinalHammer[i + 1][j + 1].upgrade.map(
+              x => x * UPGRADE_PERCENTAGE,
+            ),
+            ...costFinalHammer[i][j + 1].upgrade.map(
+              x => x * (1 - UPGRADE_PERCENTAGE),
+            ),
+            1,
+          ];
+          costFinalHammer[i][j].white = [
+            ...costFinalHammer[i + 1][j + 1].white.map(
+              x => x * UPGRADE_PERCENTAGE,
+            ),
+            ...costFinalHammer[i][j + 1].white.map(
+              x => x * (1 - UPGRADE_PERCENTAGE),
+            ),
+          ];
+          costFinalHammer[i][j].innocent = [
+            ...costFinalHammer[i + 1][j + 1].innocent.map(
+              x => x * UPGRADE_PERCENTAGE,
+            ),
+            ...costFinalHammer[i][j + 1].innocent.map(
+              x => x * (1 - UPGRADE_PERCENTAGE),
+            ),
+          ];
+          costFinalHammer[i][j].hammer = [
+            ...costFinalHammer[i + 1][j + 1].hammer.map(
+              x => x * UPGRADE_PERCENTAGE,
+            ),
+            ...costFinalHammer[i][j + 1].hammer.map(
+              x => x * (1 - UPGRADE_PERCENTAGE),
+            ),
+          ];
         }
       }
       if (actionMatrixHammer[i][j] === 'I') {
-        costFinalHammer[i][j].upgrade =
-          costNormal[0][0].upgrade / (1 - innocentNormal[0][0]);
-        costFinalHammer[i][j].white =
-          costNormal[0][0].white / (1 - innocentNormal[0][0]);
-        costFinalHammer[i][j].innocent =
-          costNormal[0][0].innocent / (1 - innocentNormal[0][0]) + 1;
-        costFinalHammer[i][j].hammer =
-          costNormal[0][0].hammer / (1 - innocentNormal[0][0]);
+        costFinalHammer[i][j].upgrade = costNormal[0][0].upgrade.map(
+          x => x / (1 - innocentNormal[0][0]),
+        );
+        costFinalHammer[i][j].white = costNormal[0][0].white.map(
+          x => x / (1 - innocentNormal[0][0]),
+        );
+        costFinalHammer[i][j].innocent = [
+          ...costNormal[0][0].innocent.map(
+            x => x / (1 - innocentNormal[0][0]) + 1,
+          ),
+          1,
+        ];
+        costFinalHammer[i][j].hammer = costNormal[0][0].hammer.map(
+          x => x / (1 - innocentNormal[0][0]),
+        );
       }
     }
   }
@@ -217,48 +282,88 @@ function calculate(upgradeLimit, actionNormal, actionHammer) {
   for (let i = upgradeLimit; i >= 0; i--) {
     for (let j = upgradeLimit; j >= i; j--) {
       if (actionMatrixNormal[i][j] === 'U') {
-        costFinalNormal[i][j].upgrade =
-          UPGRADE_PERCENTAGE * costFinalNormal[i + 1][j + 1].upgrade +
-          (1 - UPGRADE_PERCENTAGE) * costFinalNormal[i][j + 1].upgrade +
-          1;
-        costFinalNormal[i][j].white =
-          UPGRADE_PERCENTAGE * costFinalNormal[i + 1][j + 1].white +
-          (1 - UPGRADE_PERCENTAGE) * costFinalNormal[i][j + 1].white;
-        costFinalNormal[i][j].innocent =
-          UPGRADE_PERCENTAGE * costFinalNormal[i + 1][j + 1].innocent +
-          (1 - UPGRADE_PERCENTAGE) * costFinalNormal[i][j + 1].innocent;
-        costFinalNormal[i][j].hammer =
-          UPGRADE_PERCENTAGE * costFinalNormal[i + 1][j + 1].hammer +
-          (1 - UPGRADE_PERCENTAGE) * costFinalNormal[i][j + 1].hammer;
+        costFinalNormal[i][j].upgrade = [
+          ...costFinalNormal[i + 1][j + 1].upgrade.map(
+            x => x * UPGRADE_PERCENTAGE,
+          ),
+          ...costFinalNormal[i][j + 1].upgrade.map(
+            x => x * (1 - UPGRADE_PERCENTAGE),
+          ),
+          1,
+        ];
+        costFinalNormal[i][j].white = [
+          ...costFinalNormal[i + 1][j + 1].white.map(
+            x => x * UPGRADE_PERCENTAGE,
+          ),
+          ...costFinalNormal[i][j + 1].white.map(
+            x => x * (1 - UPGRADE_PERCENTAGE),
+          ),
+        ];
+        costFinalNormal[i][j].innocent = [
+          ...costFinalNormal[i + 1][j + 1].innocent.map(
+            x => x * UPGRADE_PERCENTAGE,
+          ),
+          ...costFinalNormal[i][j + 1].innocent.map(
+            x => x * (1 - UPGRADE_PERCENTAGE),
+          ),
+        ];
+        costFinalNormal[i][j].hammer = [
+          ...costFinalNormal[i + 1][j + 1].hammer.map(
+            x => x * UPGRADE_PERCENTAGE,
+          ),
+          ...costFinalNormal[i][j + 1].hammer.map(
+            x => x * (1 - UPGRADE_PERCENTAGE),
+          ),
+        ];
       }
       if (actionMatrixNormal[i][j] === 'H') {
-        costFinalNormal[i][j].upgrade =
-          HAMMER_PERCENTAGE * costFinalHammer[i][j].upgrade +
-          (1 - HAMMER_PERCENTAGE) * costFinalHammer[i][j + 1].upgrade;
-        costFinalNormal[i][j].white =
-          HAMMER_PERCENTAGE * costFinalHammer[i][j].white +
-          (1 - HAMMER_PERCENTAGE) * costFinalHammer[i][j + 1].white;
-        costFinalNormal[i][j].innocent =
-          HAMMER_PERCENTAGE * costFinalHammer[i][j].innocent +
-          (1 - HAMMER_PERCENTAGE) * costFinalHammer[i][j + 1].innocent;
-        costFinalNormal[i][j].hammer =
-          HAMMER_PERCENTAGE * costFinalHammer[i][j].hammer +
-          (1 - HAMMER_PERCENTAGE) * costFinalHammer[i][j + 1].hammer +
-          1;
+        costFinalNormal[i][j].upgrade = [
+          ...costFinalHammer[i][j].upgrade.map(x => x * HAMMER_PERCENTAGE),
+          ...costFinalHammer[i][j + 1].upgrade.map(
+            x => x * (1 - HAMMER_PERCENTAGE),
+          ),
+        ];
+        costFinalNormal[i][j].white = [
+          ...costFinalHammer[i][j].white.map(x => x * HAMMER_PERCENTAGE),
+          ...costFinalHammer[i][j + 1].white.map(
+            x => x * (1 - HAMMER_PERCENTAGE),
+          ),
+        ];
+        costFinalNormal[i][j].innocent = [
+          ...costFinalHammer[i][j].innocent.map(x => x * HAMMER_PERCENTAGE),
+          ...costFinalHammer[i][j + 1].innocent.map(
+            x => x * (1 - HAMMER_PERCENTAGE),
+          ),
+        ];
+        costFinalNormal[i][j].hammer = [
+          ...costFinalHammer[i][j].hammer.map(x => x * HAMMER_PERCENTAGE),
+          ...costFinalHammer[i][j + 1].hammer.map(
+            x => x * (1 - HAMMER_PERCENTAGE),
+          ),
+          1,
+        ];
       }
       if (actionMatrixNormal[i][j] === 'I') {
-        costFinalNormal[i][j].upgrade =
-          costNormal[0][0].upgrade / (1 - innocentNormal[0][0]);
-        costFinalNormal[i][j].white =
-          costNormal[0][0].white / (1 - innocentNormal[0][0]);
-        costFinalNormal[i][j].innocent =
-          costNormal[0][0].innocent / (1 - innocentNormal[0][0]) + 1;
-        costFinalNormal[i][j].hammer =
-          costNormal[0][0].hammer / (1 - innocentNormal[0][0]);
+        costFinalNormal[i][j].upgrade = costNormal[0][0].upgrade.map(
+          x => x / (1 - innocentNormal[0][0]),
+        );
+        costFinalNormal[i][j].white = costNormal[0][0].white.map(
+          x => x / (1 - innocentNormal[0][0]),
+        );
+        costFinalNormal[i][j].innocent = [
+          ...costNormal[0][0].innocent.map(
+            x => x / (1 - innocentNormal[0][0]) + 1,
+          ),
+          1,
+        ];
+        costFinalNormal[i][j].hammer = costNormal[0][0].hammer.map(
+          x => x / (1 - innocentNormal[0][0]),
+        );
       }
     }
   }
 
+  console.log(costFinalNormal[0][0].hammer.reduce((sum, x) => sum + x, 0))
   return costFinalNormal[0][0];
 }
 
@@ -355,4 +460,4 @@ const res = calculate(
   ],
 );
 
-console.log(res);
+// console.log(res);
